e2c736ac322dcc978f69873bd3cd1f1f9aaaeaec
Branislav Goga <brano.goga@centrum.sk>
2021-12-12 16:24:29 +0100

    Rewrite to Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,14 @@ const Navigation = (
   </Navbar>
 )
 
-function TaskListItem(props) {
+type TaskListItemProps = {
+  id: number;
+  title: string;
+  created_at: string;
+  description: string;
+}
+
+function TaskListItem(props: TaskListItemProps) {
   return <ListGroup.Item key={props.id}>
     <div className="d-flex w-100 justify-content-between">
       <h5 className="mb-1">{props.title}</h5>
@@ -25,35 +32,39 @@ function TaskListItem(props) {
   </ListGroup.Item>
 }
 
-function TaskList(props) { 
+type TaskListProps = {
+  tasks: Array<TaskListItemProps>;
+}
+
+function TaskList(props: TaskListProps) { 
 
   const tasks = createTasks()
-  let items = tasks.map(task =>  <TaskListItem {...task} />)
+  let items = tasks.map(task =>  <TaskListItem key={task.id} { ...task} />)
 
   return <ListGroup>
     {items}
   </ListGroup>
 }
 
-function createTasks() {
+function createTasks(): Array<TaskListItemProps> {
   const task1 = {
     id: 1,
     title: "Create TODO list app in different flanguages",
-    created: "3 days ago",
+    created_at: "3 days ago",
     description: "Create TODO list app in React, PHP + Nette, Python + Flash, Java Spring, ...",
   }
 
   const task2 = {
     id: 2,
     title: "Task 2",
-    created: "4 days ago",
+    created_at: "4 days ago",
     description: "Do something usefull ...",
   }
 
   const task3 = {
     id: 3,
-    title: "Task 3",
-    created: "5 days ago",
+    title: "Task 4",
+    created_at: "5 days ago",
     description: "Do something usefull ...",
   }
 
@@ -69,7 +80,7 @@ function App() {
       {Navigation}
       <Container>
         <h1>My tasks</h1>
-        <TaskList />
+        <TaskList tasks={createTasks()} />
       </Container>
     </div>
   );
